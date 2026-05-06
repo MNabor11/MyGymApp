dbade1b54a0ee6d9cff9a6528511b04f1f96587f
MNabor11 <nabormateo07@gmail.comm>
2026-05-06 01:32:41 -0400

    Teaked small thing in the texts

--- GymAppVer1.py
+++ GymAppVer1.py
@@ -10,14 +10,14 @@ print(f'A total of {exercise_Weight*exercise_Set}')
 
 
 def exerciseFunction():
-    exercise_Type = input('What workout did you do? ')
+    exercise_Type = input('\nWhat workout did you do? ')
     exercise_Set = int(input('How many sets did you do? '))
     exercise_Rep = int(input('For how many repetitions? '))
     exercise_Weight = float(input('How much weight did you use? '))
     print(
-        f'Awesome, you did {exercise_Type} for {exercise_Set} sets of {exercise_Rep}, for {exercise_Weight} lbs.')
+        f'\nAwesome, you did {exercise_Type} for {exercise_Set} sets of {exercise_Rep}, for {exercise_Weight} lbs.')
     totalWeight = (exercise_Set*exercise_Rep)*exercise_Weight
-    print(f'A total of {totalWeight} lbs.')
+    print(f'A total of {totalWeight} lbs.\n')
     return {
         'Workout': exercise_Type,
         'Sets': exercise_Set,
@@ -38,7 +38,9 @@ while (True):
         case 2:
             for x in workoutList:
                 print(
-                    f'\n{x['Workout']} for {x['Sets']} sets of {x['reps']} and {x['Weight']}\n')
+                    f'\n{x['Workout']} for {x['Sets']} sets of {x['reps']} and {x['Weight']}lbs.\n')
         case 3:
             print('Exiting program...')
             break
+        case _:
+            print("Not a valid option,\nplease choose a valid option")
